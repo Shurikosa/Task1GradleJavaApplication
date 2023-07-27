304c9037ec50c784d1d5bf6eb8b78afe80acf99c
Shurikosa <shurikosaula@gmail.com>
2023-07-27 18:44:27 +0300

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("java")
+    id("application")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.mockito:mockito-all:1.10.19")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
